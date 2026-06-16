7e376e6b6eb7776e42f0c945da02e06f74c298d3
Sardor <sardor.tozhiev1994@gmail.com>
2026-06-16 15:34:48 +0300

    fixed

--- services/movie_session.py
+++ services/movie_session.py
@@ -26,7 +26,7 @@ def get_movies_sessions(session_date: str | None = None) -> QuerySet:
     return sessions
 
 
-def get_movie_session_by_id(movie_session_id: int) -> None:
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
