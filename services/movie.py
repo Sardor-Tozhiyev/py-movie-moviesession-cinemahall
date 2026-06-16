193a47e0ac6b1dbf12bf4a3ebb09d5c078f8a884
Sardor <sardor.tozhiev1994@gmail.com>
2026-06-16 15:26:25 +0300

    fixed

--- services/movie.py
+++ services/movie.py
@@ -3,7 +3,10 @@ from django.db.models import QuerySet
 from db.models import Movie
 
 
-def get_movies(genres_ids: list | None = None, actors_ids: list | None = None) -> QuerySet:
+def get_movies(
+        genres_ids: list | None = None,
+        actors_ids: list | None = None
+) -> QuerySet:
     movies = Movie.objects.all()
 
     if genres_ids:
